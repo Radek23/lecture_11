53a7269485c011ec268b25f750e58b0d55e892a5
Radek Gláser <glaser.radek@gmail.com>
2022-05-02 09:24:24 +0200

    Add fibo.py

--- fibo.py
+++ fibo.py
@@ -1,7 +1,20 @@
+def recursive_nth_fibo(n):
+
+    if n == 0:
+        return 0
+
+    elif n == 1:
+        return 1
+
+    else:
+        return recursive_nth_fibo(n - 1) + recursive_nth_fibo(n - 2)
+
+
+
 
 def main():
-    pass
-
+    vstup = input("Zadej pocet prvku:")
+    print(recursive_nth_fibo(int(vstup)))
 
 if __name__ == "__main__":
     main()
